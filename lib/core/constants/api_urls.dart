0f727bc4722bcff0576e40f0f5d549fe11b764f2
developerSajib88 <developersajib88@gmail.com>
2025-10-25 21:43:21 +0600

    :art: [feat][products]: Api url class created for keep api necessary api urls.

--- lib/core/constants/api_urls.dart
+++ lib/core/constants/api_urls.dart
@@ -7,17 +7,15 @@ class ApiUrls {
   /// Staging Base Url
   //static const String _baseURL = "http://192.168.20.244:9090";
   /// Production Base Url
-  static const String _baseURL = "https://api.arafah.shop";
+  static const String _baseURL = "https://dummyjson.com";
 
-  static const String _imageBaseURL = "https://arafah-files.s3.ap-south-1.amazonaws.com/images/";
 
   /// Products Endpoints
-  static const String products = "/api/products";
+  static const String products = "/products";
 
 
   // Public getter to access the base URL
   static String get baseURL => _baseURL;
-  static String get imageBaseURL => _imageBaseURL;
 
   // Pass without access token
   static List<String> basicAuthUrls = [];
