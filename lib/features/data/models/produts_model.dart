148c11508e6404875e38892e7a623f4dbe05ccaa
developerSajib88 <developersajib88@gmail.com>
2025-10-25 21:50:46 +0600

    :sparkles: [fetch][products]: Products data model has created.

--- lib/features/data/models/produts_model.dart
+++ lib/features/data/models/produts_model.dart
@@ -1,32 +1,31 @@
 import 'dart:convert';
 
-import 'package:arafah/features/shop/domain/entities/products_entity.dart';
-
+import 'package:cifarx_task/features/domain/entities/products_entity.dart';
 
 class ProductsModel {
-  final ProductsData? productsData;
-  final String? message;
-  final dynamic stack;
-  final int? statusCode;
+  final List<Product>? products;
+  final int? total;
+  final int? skip;
+  final int? limit;
 
   ProductsModel({
-    this.productsData,
-    this.message,
-    this.stack,
-    this.statusCode,
+    this.products,
+    this.total,
+    this.skip,
+    this.limit,
   });
 
   ProductsModel copyWith({
-    ProductsData? productsData,
-    dynamic message,
-    dynamic stack,
-    int? statusCode,
+    List<Product>? products,
+    int? total,
+    int? skip,
+    int? limit,
   }) =>
       ProductsModel(
-        productsData: productsData ?? this.productsData,
-        message: message ?? this.message,
-        stack: stack ?? this.stack,
-        statusCode: statusCode ?? this.statusCode,
+        products: products ?? this.products,
+        total: total ?? this.total,
+        skip: skip ?? this.skip,
+        limit: limit ?? this.limit,
       );
 
   factory ProductsModel.fromRawJson(String str) => ProductsModel.fromJson(json.decode(str));
@@ -34,137 +33,126 @@ class ProductsModel {
   String toRawJson() => json.encode(toJson());
 
   factory ProductsModel.fromJson(Map<String, dynamic> json) => ProductsModel(
-    productsData: json["data"] == null ? null : ProductsData.fromJson(json["data"]),
-    message: json["message"],
-    stack: json["stack"],
-    statusCode: json["statusCode"],
+    products: json["products"] == null ? [] : List<Product>.from(json["products"]!.map((x) => Product.fromJson(x))),
+    total: json["total"],
+    skip: json["skip"],
+    limit: json["limit"],
   );
 
   Map<String, dynamic> toJson() => {
-    "data": productsData?.toJson(),
-    "message": message,
-    "stack": stack,
-    "statusCode": statusCode,
+    "products": products == null ? [] : List<dynamic>.from(products!.map((x) => x.toJson())),
+    "total": total,
+    "skip": skip,
+    "limit": limit,
   };
+
 
   ProductsEntity toEntity()=> ProductsEntity(
-    productsData: productsData,
-    message: message,
-    stack: stack,
-    statusCode: statusCode,
+    products: products,
+    total: total,
+    skip: skip,
+    limit: limit,
   );
 
-}
 
-class ProductsData {
-  final List<Product>? products;
-  final Meta? meta;
-
-  ProductsData({
-    this.products,
-    this.meta,
-  });
-
-  ProductsData copyWith({
-    List<Product>? products,
-    Meta? meta,
-  }) =>
-      ProductsData(
-        products: products ?? this.products,
-        meta: meta ?? this.meta,
-      );
-
-  factory ProductsData.fromRawJson(String str) => ProductsData.fromJson(json.decode(str));
-
-  String toRawJson() => json.encode(toJson());
-
-  factory ProductsData.fromJson(Map<String, dynamic> json) => ProductsData(
-    products: json["items"] == null ? [] : List<Product>.from(json["items"]!.map((x) => Product.fromJson(x))),
-    meta: json["meta"] == null ? null : Meta.fromJson(json["meta"]),
-  );
-
-  Map<String, dynamic> toJson() => {
-    "items": products == null ? [] : List<dynamic>.from(products!.map((x) => x.toJson())),
-    "meta": meta?.toJson(),
-  };
 }
 
 class Product {
-  final String? id;
-  final String? seller;
-  final String? name;
+  final int? id;
+  final String? title;
   final String? description;
-  final List<String>? images;
-  final int? totalStock;
-  final double? averageRating;
-  final int? numReviews;
   final String? category;
-  final String? subCategory;
-  final String? status;
-  final double? minPrice;
-  final double? baseMinPrice;
-  final int? discount;
-  final String? slug;
-  final bool? isFreeDelivery;
-  final DateTime? createdAt;
+  final double? price;
+  final double? discountPercentage;
+  final double? rating;
+  final int? stock;
+  final List<String>? tags;
+  final String? brand;
+  final String? sku;
+  final int? weight;
+  final Dimensions? dimensions;
+  final String? warrantyInformation;
+  final String? shippingInformation;
+  final String? availabilityStatus;
+  final List<Review>? reviews;
+  final String? returnPolicy;
+  final int? minimumOrderQuantity;
+  final Meta? meta;
+  final List<String>? images;
+  final String? thumbnail;
 
   Product({
     this.id,
-    this.seller,
-    this.name,
+    this.title,
     this.description,
-    this.images,
-    this.totalStock,
-    this.averageRating,
-    this.numReviews,
     this.category,
-    this.subCategory,
-    this.status,
-    this.minPrice,
-    this.baseMinPrice,
-    this.discount,
-    this.slug,
-    this.isFreeDelivery,
-    this.createdAt,
+    this.price,
+    this.discountPercentage,
+    this.rating,
+    this.stock,
+    this.tags,
+    this.brand,
+    this.sku,
+    this.weight,
+    this.dimensions,
+    this.warrantyInformation,
+    this.shippingInformation,
+    this.availabilityStatus,
+    this.reviews,
+    this.returnPolicy,
+    this.minimumOrderQuantity,
+    this.meta,
+    this.images,
+    this.thumbnail,
   });
 
   Product copyWith({
-    String? id,
-    String? seller,
-    String? name,
+    int? id,
+    String? title,
     String? description,
-    List<String>? images,
-    int? totalStock,
-    double? averageRating,
-    int? numReviews,
     String? category,
-    String? subCategory,
-    String? status,
-    double? minPrice,
-    double? baseMinPrice,
-    int? discount,
-    String? slug,
-    bool? isFreeDelivery,
-    DateTime? createdAt,
+    double? price,
+    double? discountPercentage,
+    double? rating,
+    int? stock,
+    List<String>? tags,
+    String? brand,
+    String? sku,
+    int? weight,
+    Dimensions? dimensions,
+    String? warrantyInformation,
+    String? shippingInformation,
+    String? availabilityStatus,
+    List<Review>? reviews,
+    String? returnPolicy,
+    int? minimumOrderQuantity,
+    Meta? meta,
+    List<String>? images,
+    String? thumbnail,
   }) =>
       Product(
         id: id ?? this.id,
-        seller: seller ?? this.seller,
-        name: name ?? this.name,
+        title: title ?? this.title,
         description: description ?? this.description,
-        images: images ?? this.images,
-        totalStock: totalStock ?? this.totalStock,
-        averageRating: averageRating ?? this.averageRating,
-        numReviews: numReviews ?? this.numReviews,
         category: category ?? this.category,
-        subCategory: subCategory ?? this.subCategory,
-        status: status ?? this.status,
-        minPrice: minPrice ?? this.minPrice,
-        baseMinPrice: baseMinPrice ?? this.baseMinPrice,
-        discount: discount ?? this.discount,
-        slug: slug ?? this.slug,
-        isFreeDelivery: isFreeDelivery ?? this.isFreeDelivery,
-        createdAt: createdAt ?? this.createdAt,
+        price: price ?? this.price,
+        discountPercentage: discountPercentage ?? this.discountPercentage,
+        rating: rating ?? this.rating,
+        stock: stock ?? this.stock,
+        tags: tags ?? this.tags,
+        brand: brand ?? this.brand,
+        sku: sku ?? this.sku,
+        weight: weight ?? this.weight,
+        dimensions: dimensions ?? this.dimensions,
+        warrantyInformation: warrantyInformation ?? this.warrantyInformation,
+        shippingInformation: shippingInformation ?? this.shippingInformation,
+        availabilityStatus: availabilityStatus ?? this.availabilityStatus,
+        reviews: reviews ?? this.reviews,
+        returnPolicy: returnPolicy ?? this.returnPolicy,
+        minimumOrderQuantity: minimumOrderQuantity ?? this.minimumOrderQuantity,
+        meta: meta ?? this.meta,
+        images: images ?? this.images,
+        thumbnail: thumbnail ?? this.thumbnail,
       );
 
   factory Product.fromRawJson(String str) => Product.fromJson(json.decode(str));
@@ -172,70 +160,119 @@ class Product {
   String toRawJson() => json.encode(toJson());
 
   factory Product.fromJson(Map<String, dynamic> json) => Product(
-    id: json["_id"],
-    seller: json["seller"],
-    name: json["name"],
+    id: json["id"],
+    title: json["title"],
     description: json["description"],
-    images: json["images"] == null ? [] : List<String>.from(json["images"]!.map((x) => x)),
-    totalStock: json["totalStock"],
-    averageRating: json["averageRating"]?.toDouble(),
-    numReviews: json["numReviews"],
     category: json["category"],
-    subCategory: json["subCategory"],
-    status: json["status"],
-    minPrice: json["minPrice"]?.toDouble(),
-    baseMinPrice: json["baseMinPrice"]?.toDouble(),
-    discount: json["discount"],
-    slug: json["slug"],
-    isFreeDelivery: json["isFreeDelivery"],
-    createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
+    price: json["price"]?.toDouble(),
+    discountPercentage: json["discountPercentage"]?.toDouble(),
+    rating: json["rating"]?.toDouble(),
+    stock: json["stock"],
+    tags: json["tags"] == null ? [] : List<String>.from(json["tags"]!.map((x) => x)),
+    brand: json["brand"],
+    sku: json["sku"],
+    weight: json["weight"],
+    dimensions: json["dimensions"] == null ? null : Dimensions.fromJson(json["dimensions"]),
+    warrantyInformation: json["warrantyInformation"],
+    shippingInformation: json["shippingInformation"],
+    availabilityStatus: json["availabilityStatus"],
+    reviews: json["reviews"] == null ? [] : List<Review>.from(json["reviews"]!.map((x) => Review.fromJson(x))),
+    returnPolicy: json["returnPolicy"],
+    minimumOrderQuantity: json["minimumOrderQuantity"],
+    meta: json["meta"] == null ? null : Meta.fromJson(json["meta"]),
+    images: json["images"] == null ? [] : List<String>.from(json["images"]!.map((x) => x)),
+    thumbnail: json["thumbnail"],
   );
 
   Map<String, dynamic> toJson() => {
-    "_id": id,
-    "seller": seller,
-    "name": name,
+    "id": id,
+    "title": title,
     "description": description,
-    "images": images == null ? [] : List<dynamic>.from(images!.map((x) => x)),
-    "totalStock": totalStock,
-    "averageRating": averageRating,
-    "numReviews": numReviews,
     "category": category,
-    "subCategory": subCategory,
-    "status": status,
-    "minPrice": minPrice,
-    "baseMinPrice": baseMinPrice,
-    "discount": discount,
-    "slug": slug,
-    "isFreeDelivery": isFreeDelivery,
-    "createdAt": createdAt?.toIso8601String(),
+    "price": price,
+    "discountPercentage": discountPercentage,
+    "rating": rating,
+    "stock": stock,
+    "tags": tags == null ? [] : List<dynamic>.from(tags!.map((x) => x)),
+    "brand": brand,
+    "sku": sku,
+    "weight": weight,
+    "dimensions": dimensions?.toJson(),
+    "warrantyInformation": warrantyInformation,
+    "shippingInformation": shippingInformation,
+    "availabilityStatus": availabilityStatus,
+    "reviews": reviews == null ? [] : List<dynamic>.from(reviews!.map((x) => x.toJson())),
+    "returnPolicy": returnPolicy,
+    "minimumOrderQuantity": minimumOrderQuantity,
+    "meta": meta?.toJson(),
+    "images": images == null ? [] : List<dynamic>.from(images!.map((x) => x)),
+    "thumbnail": thumbnail,
+  };
+}
+
+class Dimensions {
+  final double? width;
+  final double? height;
+  final double? depth;
+
+  Dimensions({
+    this.width,
+    this.height,
+    this.depth,
+  });
+
+  Dimensions copyWith({
+    double? width,
+    double? height,
+    double? depth,
+  }) =>
+      Dimensions(
+        width: width ?? this.width,
+        height: height ?? this.height,
+        depth: depth ?? this.depth,
+      );
+
+  factory Dimensions.fromRawJson(String str) => Dimensions.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Dimensions.fromJson(Map<String, dynamic> json) => Dimensions(
+    width: json["width"]?.toDouble(),
+    height: json["height"]?.toDouble(),
+    depth: json["depth"]?.toDouble(),
+  );
+
+  Map<String, dynamic> toJson() => {
+    "width": width,
+    "height": height,
+    "depth": depth,
   };
 }
 
 class Meta {
-  final int? page;
-  final int? limit;
-  final int? total;
-  final int? totalPages;
+  final DateTime? createdAt;
+  final DateTime? updatedAt;
+  final String? barcode;
+  final String? qrCode;
 
   Meta({
-    this.page,
-    this.limit,
-    this.total,
-    this.totalPages,
+    this.createdAt,
+    this.updatedAt,
+    this.barcode,
+    this.qrCode,
   });
 
   Meta copyWith({
-    int? page,
-    int? limit,
-    int? total,
-    int? totalPages,
+    DateTime? createdAt,
+    DateTime? updatedAt,
+    String? barcode,
+    String? qrCode,
   }) =>
       Meta(
-        page: page ?? this.page,
-        limit: limit ?? this.limit,
-        total: total ?? this.total,
-        totalPages: totalPages ?? this.totalPages,
+        createdAt: createdAt ?? this.createdAt,
+        updatedAt: updatedAt ?? this.updatedAt,
+        barcode: barcode ?? this.barcode,
+        qrCode: qrCode ?? this.qrCode,
       );
 
   factory Meta.fromRawJson(String str) => Meta.fromJson(json.decode(str));
@@ -243,16 +280,67 @@ class Meta {
   String toRawJson() => json.encode(toJson());
 
   factory Meta.fromJson(Map<String, dynamic> json) => Meta(
-    page: json["page"],
-    limit: json["limit"],
-    total: json["total"],
-    totalPages: json["totalPages"],
+    createdAt: json["createdAt"] == null ? null : DateTime.parse(json["createdAt"]),
+    updatedAt: json["updatedAt"] == null ? null : DateTime.parse(json["updatedAt"]),
+    barcode: json["barcode"],
+    qrCode: json["qrCode"],
   );
 
   Map<String, dynamic> toJson() => {
-    "page": page,
-    "limit": limit,
-    "total": total,
-    "totalPages": totalPages,
+    "createdAt": createdAt?.toIso8601String(),
+    "updatedAt": updatedAt?.toIso8601String(),
+    "barcode": barcode,
+    "qrCode": qrCode,
+  };
+}
+
+class Review {
+  final int? rating;
+  final String? comment;
+  final DateTime? date;
+  final String? reviewerName;
+  final String? reviewerEmail;
+
+  Review({
+    this.rating,
+    this.comment,
+    this.date,
+    this.reviewerName,
+    this.reviewerEmail,
+  });
+
+  Review copyWith({
+    int? rating,
+    String? comment,
+    DateTime? date,
+    String? reviewerName,
+    String? reviewerEmail,
+  }) =>
+      Review(
+        rating: rating ?? this.rating,
+        comment: comment ?? this.comment,
+        date: date ?? this.date,
+        reviewerName: reviewerName ?? this.reviewerName,
+        reviewerEmail: reviewerEmail ?? this.reviewerEmail,
+      );
+
+  factory Review.fromRawJson(String str) => Review.fromJson(json.decode(str));
+
+  String toRawJson() => json.encode(toJson());
+
+  factory Review.fromJson(Map<String, dynamic> json) => Review(
+    rating: json["rating"],
+    comment: json["comment"],
+    date: json["date"] == null ? null : DateTime.parse(json["date"]),
+    reviewerName: json["reviewerName"],
+    reviewerEmail: json["reviewerEmail"],
+  );
+
+  Map<String, dynamic> toJson() => {
+    "rating": rating,
+    "comment": comment,
+    "date": date?.toIso8601String(),
+    "reviewerName": reviewerName,
+    "reviewerEmail": reviewerEmail,
   };
 }
