5bdf1573279fcfaa502e397c4dba4fc4d9facab9
developerSajib88 <developersajib88@gmail.com>
2025-10-25 21:49:52 +0600

    :sparkles: [ui][widget]: Some re-usable widgets created.

---
+++ lib/widgets/text_forms/search_text_form.dart
@@ -0,0 +1,46 @@
+
+import 'package:cifarx_task/core/utils/styles/app_colors.dart';
+import 'package:cifarx_task/core/utils/styles/app_text_styles.dart';
+import 'package:cifarx_task/core/utils/ui_helpers/radius.dart';
+import 'package:flutter/material.dart';
+
+class SearchTextForm extends StatelessWidget {
+  final TextEditingController controller;
+  final VoidCallback onSearch;
+
+  const SearchTextForm({
+    super.key,
+    required this.controller,
+    required this.onSearch
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      cursorColor: AppColors.primaryColor,
+      decoration: InputDecoration(
+        hintText: "Search product",
+        hintStyle: AppTextStyles.primary.copyWith(color: AppColors.grey),
+        filled: true,
+        fillColor: AppColors.greyLight,
+        suffixIcon: IconButton(
+            onPressed: onSearch,
+            icon: Icon(Icons.search)
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: radius8,
+          borderSide: BorderSide(style: BorderStyle.none),
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: radius8,
+          borderSide: BorderSide(style: BorderStyle.none),
+        ),
+        border: OutlineInputBorder(
+          borderRadius: radius8,
+          borderSide: BorderSide(style: BorderStyle.none),
+        ),
+      ),
+    );
+  }
+}
